73b8a75c34041097ff908c17417fd1e2b2b9b73e
yabuki829 <syodai829@icloud.com>
2023-09-23 17:54:54 +0900

    fix

--- netlify/functions/get_book_rank.js
+++ netlify/functions/get_book_rank.js
@@ -13,18 +13,17 @@ exports.handler = async function(event, context) {
     // APIからデータを取得する
     // const response = await axios.get('http://127.0.0.1:8000/api/ranking');
     const response = await axios.get("https://bookrank-render.onrender.com/api/ranking");
-    
+    console.log("取得成功")
     const books = response.data;
     
     // 取得したデータを親ディレクトリのbooks.jsに保存する
     fs.writeFileSync(path.join(__dirname, '..', 'books.js'), JSON.stringify(books, null, 2));
-    console.log("取得に成功しました")
+    console.log("保存に成功しました")
     return {
       statusCode: 200,
       body: "Data fetched and saved successfully"
     };
   } catch (error) {
-    console.log(error)
     console.log("取得に失敗しました")
     return {
       statusCode: 500,
--- netlify/functions/test-function.js
+++ netlify/functions/test-function.js
@@ -5,27 +5,10 @@ exports.handler = async function(event, context) {
   //api通信を行う
   // books.jsとして取得したデータをjson形式にして保存する
 
-  console.log("API通信を行います")
-  try {
-    // APIからデータを取得する
-    const response = await axios.get('http://127.0.0.1:8000/api/ranking');
-    const books = response.data;
-
-    // 取得したデータを親ディレクトリのbooks.jsに保存する
-    fs.writeFileSync(path.join(__dirname, '..', 'books.js'), JSON.stringify(books, null, 2));
-    console.log("取得に成功しました")
-    return {
+  console.log("定期的に呼ばれるメソッド")
+  return {
       statusCode: 200,
-      body: "Data fetched and saved successfully"
-    };
-  } catch (error) {
-    console.log(error)
-    console.log("取得に失敗しました")
-    return {
-      statusCode: 500,
-      body: `Error fetching data: ${error}`
-    };
-  }
+  };
 };
 
 // ドキュメント
