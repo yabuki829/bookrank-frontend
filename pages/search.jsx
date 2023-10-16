1562e96dd2c36109bbb148a07ad6cc1c423836ee
yabuki829 <syodai829@icloud.com>
2023-10-16 13:33:20 +0900

    fix

--- pages/search.jsx
+++ pages/search.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import books_data from '../books'
 import Header from '../components/layouts/Header'
-
+import Head from 'next/head';
 const Search = () => {
 
   const [searchTerm, setSearchTerm] = useState('')
